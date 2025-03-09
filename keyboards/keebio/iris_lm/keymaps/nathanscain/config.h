a361fff3c2a489f33d44cd10b26b5f2c5b8b3350
Nathan Cain <nathan@nathancain.dev>
2025-03-09 01:15:47 -0600

    iris_lm home row mods

---
+++ keyboards/keebio/iris_lm/keymaps/nathanscain/config.h
@@ -0,0 +1,9 @@
+// Copyright 2025 Nathan Cain (@nathanscain)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TAPPING_TERM 240
+#define PERMISSIVE_HOLD
+#define CHORDAL_HOLD
+#define QUICK_TAPPING_TERM 120
--- keyboards/keebio/iris_lm/keymaps/nathanscain/keymap.c
+++ keyboards/keebio/iris_lm/keymaps/nathanscain/keymap.c
@@ -1,12 +1,14 @@
-// Copyright 2023 Danny Nguyen (@nooges)
+// Copyright 2025 Nathan Cain (@nathanscain)
 // SPDX-License-Identifier: GPL-2.0-or-later
 
 #include QMK_KEYBOARD_H
 
+#include "keydefs.h"
+
 enum custom_layers {
      _QWERTY,
      _LOWER,
-     _RAISE
+     _RAISE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -17,7 +19,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+     KC_LCTL, GUI_A,   ALT_S,   SFT_D,   CTL_F,   KC_G,                               KC_H,    CTL_J,   SFT_K,   ALT_L,   GUI_SCLN,KC_QUOT,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
      KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_HOME,          KC_END,  KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
