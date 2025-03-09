fef5e9dc5cc7c2ef55ec58c878ae24116d70b9d7
Nathan Cain <nathan@nathancain.dev>
2025-03-09 01:15:47 -0600

    iris_lm home row mods

---
+++ keyboards/keebio/iris_lm/keymaps/nathanscain/keydefs.h
@@ -0,0 +1,14 @@
+// Copyright 2025 Nathan Cain (@nathanscain)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+// home row mod-taps
+#define GUI_A LGUI_T(KC_A)
+#define ALT_S LALT_T(KC_S)
+#define SFT_D LSFT_T(KC_D)
+#define CTL_F LCTL_T(KC_F)
+#define CTL_J RCTL_T(KC_J)
+#define SFT_K RSFT_T(KC_K)
+#define ALT_L RALT_T(KC_L)
+#define GUI_SCLN RGUI_T(KC_SCLN)
